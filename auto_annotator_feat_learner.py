d34f6553558e9238b199b99acc604931303a7200
Jack Geary <s1674364@sms.ed.ac.uk>
2017-07-07 20:09:04 +0100

    Nothing quite working. Looking at PCs of Isomap

--- auto_annotator_feat_learner.py
+++ auto_annotator_feat_learner.py
@@ -48,30 +48,39 @@ class DataStore():
 class ChangeLearner():
 
     def __init__(self,sample_state):
-        self.prev_state = None
+        self.prev_state = [0 for _ in range(len(sample_state))]
         self.cur_state = sample_state
 
         self.alpha = .01
         self.gamma = 1.0
 
         self.epsilon = .05
-        self.num_rounds = 0
+        self.num_rounds = {"F":0,"L":0,"R":0}
 
+        #The set of actions available
         self.action_list = ["F","L","R"]
-        self.action_dict = {}
-        for act in self.action_list: self.action_dict[act] = [0 for _ in range(len(self.cur_state))]
+
+        self.mag_list = []
+
+        #For each action we want to store the average state change induced by that action
+        #And the set of weights corresponding to that action
+        self.action_state = {}
+        self.action_weight = {}
+        for act in self.action_list: 
+            self.action_state[act] = [0 for _ in range(len(self.cur_state))]
+            self.action_weight[act] = [1.0/len(self.cur_state) for _ in range(len(self.cur_state))]
 
         #Here we are going to use our knowledge of the current structure of the state to try and influence 
         #We know right should tend to be positve prp-acceleration and left negative. By intitialising these
         #Values towards that direction we make it more likely they will be selected
         random.seed(270394)
-        self.action_dict["R"][3] = math.fabs(random.random())
-        self.action_dict["L"][3] = -math.fabs(random.random())
+        self.action_state["R"][3] = math.fabs(random.random())
+        self.action_state["L"][3] = -math.fabs(random.random())
 
         #Variables for the time dilation
-        self.capacity = 50
-        self.num_reps = 500
-        self.entry_window = []
+        #self.capacity = 50
+        #self.num_reps = 500
+        #self.entry_window = []
 
 
     def updateState(self,new_state):
@@ -80,23 +89,46 @@ class ChangeLearner():
 
 
     def learn(self,action,difference):
+        weight_sum = 0
+        self.num_rounds[action]+=1
+        num_rounds = self.num_rounds[action]
         for i,entry in enumerate(difference):
-            self.action_dict[action][i] += (self.alpha+(1.0/self.num_rounds))*(self.gamma*entry-self.action_dict[action][i])
+            self.action_weight[action][i] += (self.alpha+(1.0/num_rounds))*entry
+            weight_sum += math.fabs(self.action_weight[action][i])
+
+        #Normalise the weights
+        self.action_weight[action] = [x/weight_sum for x in self.action_weight[action]]
+
+        #Use moving average to keep track of the average change induced by an action
+        self.action_state[action]  = [((num_rounds-1.0)/num_rounds)*x + (1.0/num_rounds)*difference[i] for i,x in enumerate(self.action_state[action])]
 
 
     def getAction(self):
-        self.num_rounds += 1     
+        approx_state = []
+        total_num_rounds = sum([self.num_rounds[x] for x in self.action_list])+1
         diff_mags = [0 for _ in range(len(self.action_list))]
-        
-        state_diff = [self.cur_state[i]-self.prev_state[i] for i in range(len(self.cur_state))]
-        for i,action in enumerate(self.action_list):
-            for j,entry in enumerate(state_diff): 
-                diff_mags[i]+= math.fabs(entry - self.action_dict[action][j])
        
+        true_change = [self.cur_state[i] - self.prev_state[i] for i in range(len(self.prev_state))]
+ 
+        #Given this setup we are trying to determine the most likely action to have just been taken
+        #state_diff = [self.cur_state[i]-self.prev_state[i] for i in range(len(self.cur_state))]
+        for i,action in enumerate(self.action_list):
+            approx_state = [self.prev_state[j]+self.action_state[action][j] for j in range(len(self.prev_state))]
+            for j,entry in enumerate(approx_state): 
+                diff_mags[i]+= math.fabs(self.action_weight[action][j]*(self.cur_state[j] - approx_state[j]))
+       
+        #Stored separately for plotting purposes
         self.mag_list = list(diff_mags)
 
+
+        # Epsilon - greedy action sampling
         alt = random.random()
-        if alt<(self.epsilon+(1.0/self.num_rounds)):
+        #Annealing epsilon so that we make fewer random guesses over time
+        if alt<(self.epsilon+(1.0/(total_num_rounds))):
+            #Here the action is sampled at random based on 1-the proportion of the 
+            # sum of the magnitudes of the difference determined by that action
+            #In this the action with the lowest magnitude difference is still
+            # most probable to be selected
             sum_mags = sum(diff_mags)
             for i in range(len(diff_mags)):
                 diff_mags[i]/=sum_mags
@@ -111,29 +143,31 @@ class ChangeLearner():
             min_mag = diff_mags[min_ind] 
         
         else:
+            #Greedy action selection. Choose action with difference of lowest magnitude
             min_mag = min(diff_mags)
             min_ind = diff_mags.index(min_mag)
         
         min_action = self.action_list[min_ind]
 
+        self.num_rounds[min_action] += 1
 
-        self.learn(min_action,state_diff)
+        self.learn(min_action,true_change)
         
-        self.entry_window.append((min_action,state_diff))
-        if len(self.entry_window) == self.capacity:
-            action,diff = None, None
-            for _ in range(self.num_reps):
-                index_list = [random.randint(0,self.capacity-1) for _ in range(self.capacity)]
-                for index in index_list:
-                    (action,diff) = self.entry_window[index]
-                    self.learn(action,diff)
-            self.entry_window = [] 
+        #self.entry_window.append((min_action,state_diff))
+        #if len(self.entry_window) == self.capacity:
+        #    action,diff = None, None
+        #    for _ in range(self.num_reps):
+        #        index_list = [random.randint(0,self.capacity-1) for _ in range(self.capacity)]
+        #        for index in index_list:
+        #            (action,diff) = self.entry_window[index]
+        #            self.learn(action,diff)
+        #    self.entry_window = [] 
         
         return min_action,min_mag
 
 
     def whatDoYouKnow(self):
-        return self.action_dict
+        return self.action_state, self.action_weight
 
 
 def advanceAll(source_list):
@@ -174,11 +208,17 @@ for i in range(len(datasource_list[0].file_content)):
 for source in datasource_list:
     source.rescale()
 
+road_posit = datastore_dict["PROC_LANE_DETECTION"].getAll(1)
+plt.plot(road_posit)
+plt.show()
+exit(-1)
+
 time = []
 action = None
 new_state = None
 
 plot_list = []
+conv_list = []
 
 state_len = 0
 for source in datasource_list:
@@ -195,17 +235,23 @@ while None not in advanceAll(datasource_list):
     learner.updateState(new_state)
     action,mag = learner.getAction()
 
+    #Keep track of the magnitude of the difference for each action at each timestep
     plot_list.append(list(learner.mag_list))
     print("{}:{:02d} ({}) \t {}\t{}".format(int(time/60),int(time%60),time,action,mag))
 
 print("\n What the Learner Learnt:")
-learnt_dict = learner.whatDoYouKnow()
-learnt_list = None
+learnt_dict,weight_dict = learner.whatDoYouKnow()
+learnt_list,weight_list = None,None
 for direc in learnt_dict:
     learnt_list = learnt_dict[direc]
+    weight_list = weight_dict[direc]
     learnt_list = [round(x,3) for x in learnt_list]
+    weight_list = [round(x,3) for x in weight_list]
     print("{}: {}".format(direc,learnt_list))
+    print("{}: {}".format(direc,weight_list))
+    print("")
 
+print(learner.num_rounds)
 
 plot_1 = [entry[0] for entry in plot_list]
 plot_2 = [entry[1] for entry in plot_list]
--- manifold_learning.py
+++ manifold_learning.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import manifold,preprocessing
+from sklearn import decomposition,manifold,preprocessing
 from sklearn.cluster import KMeans #used to perform k-means
 from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.cluster.hierarchy import fcluster
@@ -22,6 +23,10 @@ class DataStore():
             if -9.0 not in entry:
                 self.file_content.append(entry)
 
+        if "PROC_LANE_DETECTION" in self.name:
+            self.rehab()
+            self.file_content = [entry[:-1] for entry in self.file_content] #Omit unrelated variable
+        
         self.index = 0 #Initialise the index to 0
         self.time = self.file_content[0][0]
 
@@ -33,15 +38,68 @@ class DataStore():
 
         return tot_list
 
-    def getRow(self):
-        return self.file_content[self.index]
+    def getRow(self,index=None):
+        if index is None: index = self.index
+        return self.file_content[index]
 
+    def getIndex(self,time=None):
+        i = 0
+        min_dist = None
+        if time is None: time=self.time
+        while min_dist is None or math.fabs(time-self.file_content[i][0])<min_dist:
+            min_dist = math.fabs(time-self.file_content[i][0])
+            i+=1
+        return i-1
+ 
     def advance(self):
         self.index+=1
         if self.index>=len(self.file_content): self.index = None
         else:
             self.time = self.file_content[self.index][0]
 
+    def reverse(self):
+        self.index-=1
+        if self.index<0: self.index = 0
+        self.time = self.file_content[self.index][0]
+
+    
+    def rehab(self):
+        start,end,direc = -1,-1,None
+        i = 0
+        entry, change = None,None
+        err_index,break_list = [],[]     
+
+        #-1 means calibrating. Assume this only happens at start of dataset
+        while self.file_content[0][-1] == -1:
+            del self.file_content[0]
+        
+        #A list of all the points where the detection software gives bad output
+        err_index = [j for j,entry in enumerate(self.file_content) if entry[-1] in [0,-1]]
+        
+        while i<len(err_index):
+            start = err_index[i]
+            while i+1<len(err_index) and err_index[i+1]-err_index[i]<5: i+=1
+            end = err_index[i]
+            break_list.append((start,end))
+            i+=1
+
+        for (a,b) in break_list:
+            start = list(self.file_content[a-1])
+            end = list(self.file_content[b+1])
+            road_width = end[3]
+ 
+            #Only need direc for car position as is discontinuous
+            direc = start[1]-self.file_content[a-2][1] #Instantaneous slope before data loss            
+            if (direc<0 and start[1]<0 and end[1]>0) or (direc>0 and start[1]>0 and end[1]<0):
+                end[1] += (direc/math.fabs(direc))*road_width #Add or subtract road width as necessary
+
+            change = [(end[x] - start[x])*1.0/((b+1)-(a-1)) for x in range(len(start))]
+            for j in range(a,b+1):
+                self.file_content[j] = [round(self.file_content[j-1][x] + change[x],3) for x in range(len(change)-1)] +[3.0]
+                if math.fabs(self.file_content[j][1])>road_width/2:
+                    self.file_content[j][1] = round(self.file_content[j][1]-(direc/math.fabs(direc))*road_width,3)            
+       
+     
 
 def advanceAll(source_list):
     index_list = []
@@ -54,10 +112,25 @@ def advanceAll(source_list):
         index_list.append(source.index)
     return index_list
 
-folder_loc = "Datasets/UAH-DRIVESET-v1/D2/20151120133502-26km-D2-AGGRESSIVE-MOTORWAY/"
+
+def advanceIncr(source_list):
+    index_list = []
+    cur_time = None
+    cur_source = None
+    for source in source_list:
+        if cur_source is None or cur_time>source.time:
+            cur_time = source.time
+            cur_source = source
+    cur_source.advance()
+    index_list = [source.index for source in source_list]    
+    return index_list
+
+
+folder_loc = "Datasets/UAH-DRIVESET-v1/D1/20151111123124-25km-D1-NORMAL-MOTORWAY/"
 
 files_of_interest = ["RAW_ACCELEROMETERS","PROC_LANE_DETECTION","PROC_VEHICLE_DETECTION"]
-entries_of_interest = [[0,2,3,4,5,6,7,8,9,10],[0,1,2,3],[0,1,2,3,4]]
+entries_of_interest = [[0,2,3,4,5,6,7,8,9,10],[0,1,2,3,4],[0,1,2,3,4]]
+#entries_of_interest = [[0,2,3,4,5,6,7,10],[0,1,2,3,4],[0,1,2,3,4]]
 
 datastore_list = []
 for i,entry in enumerate(files_of_interest):
@@ -65,83 +138,159 @@ for i,entry in enumerate(files_of_interest):
 
 dataset = []
 row_list = []
+
+advanceAll(datastore_list) #Bring all sources up to a common starting point
+
 for entry in datastore_list: row_list += entry.getRow()[1:]
 dataset.append([min(x.time for x in datastore_list)]+row_list)
 
-while None not in advanceAll(datastore_list):
+while None not in advanceIncr(datastore_list):
     row_list = []
     for entry in datastore_list: row_list+= entry.getRow()[1:]
     dataset.append([min([x.time for x in datastore_list])]+row_list)
 
+last,ind = 0,1
+granularity = .5
+
+for i in range(len(dataset)):
+    if dataset[i][0]%granularity<granularity/2:
+        dataset[i][0]-=dataset[i][0]%granularity
+    else:
+        dataset[i][0]+= granularity - dataset[i][0]%granularity
+
+while ind+1<len(dataset):
+    while ind<len(dataset) and dataset[ind][0]-dataset[last][0]<granularity:
+        del dataset[ind]
+
+    last = ind
+    ind += 1
+
+extra_vals = None
+prev_accl = [0,0,0]
+prev_accl_kal = [0,0,0]
+prev_angles = [0,0,0]
+car_width = 1.75
+
+
+car_rat = None
+
+for i,entry in enumerate(dataset):
+    extra_vals = []
+    extra_vals.append(entry[1]-prev_accl[0])
+    extra_vals.append(entry[2]-prev_accl[1])
+    extra_vals.append(entry[3]-prev_accl[2])
+
+    extra_vals.append(entry[4]-prev_accl_kal[0])
+    extra_vals.append(entry[5]-prev_accl_kal[1])
+    extra_vals.append(entry[6]-prev_accl_kal[2])
+
+    extra_vals.append(entry[7]-prev_angles[0])
+    extra_vals.append(entry[8]-prev_angles[1])
+    extra_vals.append(entry[9]-prev_angles[2])
+
+
+    road_width = entry[12]
+    z_t = entry[10]
+    z_l = (road_width/2)+z_t-(car_width/2)
+    z_r = (road_width/2)-z_t-(car_width/2) 
+    extra_vals.append(z_l/z_r)
+
+    dataset[i]+=extra_vals
+
 posit=0
-window_len = 30
+time_width = 1 #seconds
+window_len =  math.ceil(time_width/granularity) #Assume time_width>granularity
 windowed_dataset = []
 while posit<len(dataset):
     row = []
     j = 0
-    while posit+j<len(dataset) and j<window_len and dataset[posit+j][0]-dataset[posit][0]<window_len:
+    while posit+j<len(dataset) and j<window_len: #dataset[posit+j][0]-dataset[posit][0]<window_len:
         row += dataset[posit+j][1:]
         j += 1
     
-    if j == window_len:    
+ 
+    if posit+j<len(dataset) and  j==window_len: #dataset[posit+j][0]-dataset[posit][0] >= window_len:    
         windowed_dataset.append([dataset[posit+j-1][0]]+row) #want to include the timestep for reference
-        posit+=int(window_len/2) #Overlapping windows
+        posit+=window_len#posit+=int(window_len/2) #Overlapping windows
     else:
         posit += j
        
-    
+   
 #dataset_np = np.array(dataset[1:])
 dataset_np = np.array([entry[1:] for entry in windowed_dataset])
 
 
 #Scales data to have 0 mean and unit variance
-scaled_dataset = preprocessing.MaxAbsScaler().fit_transform(dataset_np)
+#scaled_dataset = preprocessing.MaxAbsScaler().fit_transform(dataset_np)
+scaled_dataset = preprocessing.StandardScaler().fit_transform(dataset_np)
+
+n_comp = 2
 
 
-Y = manifold.Isomap(n_neighbors = 5,n_components = 2).fit_transform(scaled_dataset)
+isMap = manifold.Isomap(n_neighbors = 10,n_components = n_comp)
+Y = isMap.fit_transform(scaled_dataset)
+#Y = isMap.fit_transform(dataset_np)
+
+t= isMap.transform(np.identity(scaled_dataset.shape[1]))
+
+
+for i in range(n_comp):
+    for j,entry in enumerate(t.T[i]):
+        print("{}: {} \t {}".format(j,entry,math.fabs(entry/sum(math.fabs(x) for x in t.T[i]))))
+    print("\n")
+    
+
 
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(Y)
 labels = kmeans.predict(Y)
 
-plt.scatter(Y[:,0],Y[:,1],c=labels.astype(np.float))
+posit = [i for i in range(len(Y))]
+
+lc = [35,55,65,83,98,104,139,164,177,208,257,272,305,371,376]
+lc_posit = []
+start,end = 0,0
+
+for entry in lc:
+    while entry> windowed_dataset[start+1][0]: start+=1
+    end = start+1
+    while entry>windowed_dataset[end+1][0]: end+=1
+
+    for i in range(start-1,end+1):
+        lc_posit.append(i)
+
+
+#for entry in lc_posit:
+#    print("{}\t{}".format(windowed_dataset[entry][0],labels[entry]))
+
+#print("\n\n")
+
+#for entry in labels:
+#    print(entry)
+
+
+#for i in range(n_comp):
+#    plt.plot(posit,Y[:,i],'b-')
+#    plt.plot(lc_posit,Y[lc_posit,i],'r.')
+#    plt.title("This is {}".format(i))
+#    plt.show()
+
+
+plt.scatter(posit,Y[:,1],c=labels.astype(np.float),cmap=plt.cm.Spectral)
+#plt.scatter(Y[:,0],Y[:,1],c=labels.astype(np.float),cmap=plt.cm.Spectral)
 plt.show()
 
-#This is the linkage used to measure the distance between clusters
-Z = linkage(Y, 'ward') 
-
-#plt.figure(figsize=(25, 10))
-plt.title('Hierarchical Clustering Dendrogram')
-plt.xlabel('sample index')
-plt.ylabel('distance')
-Z_tree = dendrogram(
-    Z,
-    p=2,
-    truncate_mode = 'level',
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=8.,  # font size for the x axis labels
-)
-
-plt.axhline(y=55, c='k')
-
-plt.show()
 
 
-k=3
-point_clas = fcluster(Z, k, criterion='maxclust')
-
-
-printable = True
-for i in range(len(dataset)):
-    window_it = 0
-    while printable and dataset[i][0]>windowed_dataset[window_it][0]:
-        window_it+=1
-        if window_it == len(windowed_dataset):
-            printable = False
-    if printable:
-        print("{}\t{}".format(i,window_it))
-        print("{}:{}\t{}\t{}".format(int(dataset[i][0]/60),int(dataset[i][0]%60),\
-                                 labels[window_it],point_clas[window_it]))
+#printable = True
+#for i in range(len(dataset)):
+#    window_it = 0
+#    while printable and dataset[i][0]>windowed_dataset[window_it][0]:
+#        window_it+=1
+#        if window_it == len(windowed_dataset):
+#            printable = False
+#    if printable:
+#        print("{}:{:02d}\t{}".format(int(dataset[i][0]/60),int(dataset[i][0]%60),labels[window_it]))
 
 #for i,entry in enumerate(dataset_np):
 #    print("{}\t{}:{}\t{}".format(i,int(entry[0]/60),int(entry[0]%60),labels[i]))
